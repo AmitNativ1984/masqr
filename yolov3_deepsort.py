8a4706306504fd17a395f9d18aeb80f46b077850
Amit Nativ <E012336@E012336N>
2020-07-02 00:22:06 +0300

    print points

--- yolov3_deepsort.py
+++ yolov3_deepsort.py
@@ -4,7 +4,7 @@ import os
 import cv2
 import time
 import argparse
-
+import matplotlib.pyplot as plt
 # import imutils
 import torch
 import warnings
@@ -142,7 +142,7 @@ class VideoTracker(object):
     def run(self):
         results = []
         idx_frame = 0
-
+        plt.figure()
         while self.vdo.grab():
 
             #########################################
@@ -218,10 +218,11 @@ class VideoTracker(object):
 
                     # Show every point on the top view image
                 for point in transformed_downoids:
+                    print(point)
                     x, y = point
-                    cv2.circle(bird_view_img, (x, y), BIG_CIRCLE, COLOR_GREEN, 2)
-                    cv2.circle(bird_view_img, (x, y), SMALL_CIRCLE, COLOR_GREEN, -1)
-
+                    # cv2.circle(bird_view_img, (x, y), BIG_CIRCLE, COLOR_GREEN, 2)
+                    # cv2.circle(bird_view_img, (x, y), SMALL_CIRCLE, COLOR_GREEN, -1)
+                    plt.scatter(x, y, marker='x', color='b')
                 list_indexes = list(itertools.combinations(range(len(transformed_downoids)), 2))
                 for i, pair in enumerate(itertools.combinations(transformed_downoids, r=2)):
                     # Check if the distance between each combination of points is less than the minimum distance chosen
@@ -294,4 +295,5 @@ if __name__ == "__main__":
     with VideoTracker(cfg, args, video_path=args.VIDEO_PATH) as vdo_trk:
         vdo_trk.run()
 
+    plt.show()
 
